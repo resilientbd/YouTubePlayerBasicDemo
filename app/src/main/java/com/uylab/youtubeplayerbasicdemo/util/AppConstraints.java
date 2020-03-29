7f7d6f30c1ef2f696551b28ced3c56c092995410
resilientbd <faisal.hossain.pk@gmail.com>
2020-03-29 23:18:02 +0600

    remove api key

--- app/src/main/java/com/uylab/youtubeplayerbasicdemo/util/AppConstraints.java
+++ app/src/main/java/com/uylab/youtubeplayerbasicdemo/util/AppConstraints.java
@@ -2,6 +2,6 @@ package com.uylab.youtubeplayerbasicdemo.util;
 
 public class AppConstraints {
     public static interface ApiKey{
-        public String API_KEY="AIzaSyCntFADcyE-EmqzTtgymjByBfnquiVc4pA";
+        public String API_KEY="YOUR API KEY SHOULD BE HERE";
     }
 }
